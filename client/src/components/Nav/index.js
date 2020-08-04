137c8bad0ff3410bd3e2c14312f3cc40ced8b1f3
pocketdru <pocketdru@gmail.com>
2020-08-03 23:23:45 -0600

    responsive size

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,19 +9,23 @@ function Nav() {
       <div className="row"> 
       <div className="col-md-12 col-lg-12 col-sm-12">
       <nav className="nav navbar d-flex">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand col-md-4 col-sm-12" href="#">
               <img src={logo} height="30" className="d-inline-block align-top" alt="" loading="lazy"/>
           </a>
-          <a className="navbar-brand mr-5 ml-0">Atlantida</a>    
-          <a className="nav-link" href="/">Home</a>
-          <a className="nav-link" href="/saved">About</a>
-          <a className="nav-link" href="/">Services</a>
-          <a className="nav-link" href="/saved">History</a>
-          <a className="nav-link" href="/saved">Team</a>
-          <a className="nav-link" href="/saved">Portfolio</a>
-          <a className="nav-link" href="/saved">Prices</a>
-          <a className="nav-link" href="/saved">Blog</a>
-          <a className="nav-link" href="/">Contact</a>
+          <span className="col-md-7">
+            <a className="navbar-brand mr-5 ml-0">Atlantida</a>    
+            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href="/saved">About</a>
+            <a className="nav-link" href="/">Services</a>
+            <a className="nav-link" href="/saved">History</a>
+          </span>
+          <span className="col-md-12">
+            <a className="nav-link" href="/saved">Team</a>
+            <a className="nav-link" href="/saved">Portfolio</a>
+            <a className="nav-link" href="/saved">Prices</a>
+            <a className="nav-link" href="/saved">Blog</a>
+            <a className="nav-link" href="/">Contact</a>
+          </span>
       </nav>
       </div>
       </div>
