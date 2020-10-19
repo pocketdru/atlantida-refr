5fd338821b5747af4cd862468ce91aa9359a9956
pocketdru <pocketdru@gmail.com>
2020-10-18 23:51:05 -0600

    wave function got to work

--- client/src/components/Waves/index.js
+++ client/src/components/Waves/index.js
@@ -2,11 +2,45 @@ import React , { Component } from "react";
 import "./style.css";
 
 class WavySvg extends Component {
+    componentDidMount () {
+        this.bigAnimate()
+    }
+
+    bigAnimate = () => {
+       let xs = [];
+        for (var i=0; i <= 500; i++) {
+            xs.push(i)
+        }
+
+        function animate () {
+            let xs = [];
+    
+            for (var i=0; i <= 500; i++) {
+                xs.push(i)
+            }
+    
+            let points = xs.map(x => {
+    
+                return [x, x]
+            })
+    
+            let path = "M" + points.map(p => {
+                return p[0] + "," + p[1]
+            }).join(" L")
+            console.log("it works")
+            requestAnimationFrame(animate)
+
+        }
+        animate()
+    }
 
     render() {
+
         return(
             <div>
-                <svg></svg>
+                <svg>
+                    <path d=""></path>
+                </svg>
             </div>
         )
     }
