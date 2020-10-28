7293f7ca2445eecb3d91d8b47044c0dcea1489a7
pocketdru <pocketdru@gmail.com>
2020-10-28 14:41:47 -0600

    board

---
+++ client/src/components/Board/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Board() {
+  return (
+    <div className="container-fluid ">
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container-fluid">
+
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default Board;
