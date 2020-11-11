8301f859b626075f28e7ebf4a1dcb3323a469943
pocketdru <pocketdru@gmail.com>
2020-11-11 00:37:27 -0700

    positioning buttons and styling

--- client/src/components/Board/index.js
+++ client/src/components/Board/index.js
@@ -6,6 +6,7 @@ function Board() {
   return (
     <div className="container board">
         <div className="row">
+          <div className="colm-md-12">
             <div className="card">
               <img src="/images/board-1.png" className="card-img-top" alt="..."/>
               <div className="card-body">
@@ -13,9 +14,10 @@ function Board() {
                 <span><WavySvg/></span>
                 </h2>
                 <p className="card-text pr-5 pl-5">Donec aliquet, ipsum ut lobortis facilisis, nisi ligula aliquam odio, ut vehicula odio lorem in erat. Quisque quam ipsum, commodo non condimentum a, fermentum id sapien. Pellentesque sit amet sem neque. Maecenas est enim, sollicitudin sit amet nisi vel, mattis efficitur sapien. Nulla lobortis dui in aliquam placerat. Maecenas commodo odio nec neque imperdiet sagittis. Fusce mauris tellus, vulputate id finibus a, rutrum et justo. Vivamus at eros sed justo laoreet mattis vulputate vel magna. Sed quis pulvinar quam, et tempus purus. Fusce et nisi eget dui hendrerit iaculis. Cras volutpat augue tempor viverra tincidunt. Maecenas interdum scelerisque enim. Aenean interdum leo ac lectus gravida, id tincidunt sem finibus. Curabitur ac arcu blandit, tempus leo id, ultrices ante. </p>
-                <button type="button" className="btn btn-outline-primary">Primary</button>
-                <button type="button" className="btn btn-outline-secondary">Secondary</button>
+                <button type="button" className="btn btn-outline-primary mx-auto">Contact Us</button>
+                <button type="button" className="btn btn-outline-secondary mx-auto">Portfolio</button>
               </div>
+          </div>
           </div>
         </div>
     </div>
