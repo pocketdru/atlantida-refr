3b8035883cd884071eddb0dc60242bccbd0d68e8
pocketdru <pocketdru@gmail.com>
2020-11-12 00:46:00 -0700

    checkbox content

--- client/src/components/Checkbox/index.js
+++ client/src/components/Checkbox/index.js
@@ -8,37 +8,37 @@ function Checkbox() {
         <div className="row row-cols-1 row-cols-md-3">
             <div className="col-md-3">
                 <div className="card">
-                <img src="..." className="card-img-top" alt="..."/>
+                <img src="/images/about/icon-1.png" className="card-img-top mx-auto mt-5" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">AWARDS WINNER</h5>
+                    <p className="card-text">54</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-3">
                 <div className="card">
-                <img src="..." className="card-img-top" alt="..."/>
+                <img src="/images/about/icon-2.png" className="card-img-top mx-auto mt-4" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">JOBS DONE</h5>
+                    <p className="card-text">1054</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-3">
                 <div className="card">
-                <img src="..." className="card-img-top" alt="..."/>
+                <img src="/images/about/icon-3.png" className="card-img-top mx-auto mt-4" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    <h5 className="card-title">HAPPY CLIENTS</h5>
+                    <p className="card-text">120</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-3">
                 <div className="card">
-                <img src="..." className="card-img-top" alt="..."/>
+                <img src="/images/about/icon-4.png" className="card-img-top mx-auto mt-4" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">reviews recieved</h5>
+                    <p className="card-text">54</p>
                 </div>
                 </div>
             </div>
