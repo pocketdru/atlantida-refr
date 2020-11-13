012543587fc9eb44a7ed59d2d6d3070bc5e634d6
pocketdru <pocketdru@gmail.com>
2020-11-13 00:23:51 -0700

    services content

---
+++ client/build/precache-manifest.d495ab76cc2bf58c8efefd90ef62e32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe98ae8f26f5fb13683fc00a5b41681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa4b46d4dc94d734b8b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b630b80f083478f7343",
+    "url": "/static/js/2.283e5d51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.283e5d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa4b46d4dc94d734b8b",
+    "url": "/static/js/main.7c650ce7.chunk.js"
+  },
+  {
+    "revision": "b3c68ffccbd578b75117",
+    "url": "/static/js/runtime-main.d60d4204.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./style.css";
+import WavySvg from "../Waves";
 
 function Services() {
   return (
     <div className="container-fluid services">
         <div className="row">
           <div className="col-md-12">
-
+            <h1>
+              SERVICES
+              <span><WavySvg/></span>
+            </h1>
           </div>
         </div>
         <div className="row"></div>
