8657d720879c6afec796a1ab98c9b421be5a4a7f
pocketdru <pocketdru@gmail.com>
2020-11-19 00:26:05 -0700

    services section added

--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -4,19 +4,20 @@ import WavySvg from "../Waves";
 
 function Services() {
   return (
-    <div className="container-fluid services pt-5 pb-5">
-        <div className=" row card bg-dark text-white">
-          <div className="col-md-12">
-        <img src="/images/services/Background.png" class="card-img" alt="..."/>
-        <div className="card-img-overlay">
-          <h2 className="card-title text-center">Services
-              <span><WavySvg/></span></h2>
-          <p className="card-text text-center">Donec aliquet, ipsum ut lobortis facilisis, nisi ligula aliquam odio, ut vehicula odio lorem in erat. Quisque quam ipsum, commodo non condimentum a, fermentum id sapien. Pellentesque sit amet sem neque. </p>
+    <section className="services">
+      <div className="container pt-5 pb-5">
+          <div className=" row card bg-dark text-white">
+            <div className="col-md-12">
+          <div className="card-img-overlay">
+            <h2 className="card-title text-center">Services
+                <span><WavySvg/></span></h2>
+            <p className="card-text text-center">Donec aliquet, ipsum ut lobortis facilisis, nisi ligula aliquam odio, ut vehicula odio lorem in erat. Quisque quam ipsum, commodo non condimentum a, fermentum id sapien. Pellentesque sit amet sem neque. </p>
+          </div>
         </div>
+        </div>
+          <div className="row"></div>
       </div>
-      </div>
-        <div className="row"></div>
-    </div>
+    </section>
   );
 }
 
