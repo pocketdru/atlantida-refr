fb6e618f1c6cae8dcac33ab46287a9b988d83af0
pocketdru <pocketdru@gmail.com>
2020-12-24 22:33:26 -0700

    history heading

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -4,7 +4,24 @@ import WavySvg from "../Waves";
  
 function History() {
     return (
-        <section className="history"></section>
+        <section className="history">
+            <div className="container pt-5 pb-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div>
+                                    <h2 className="card-title text-center">History
+                                    <span><WavySvg/></span></h2>
+                                    <p className="card-text text-center">In pulvinar, mi at rhoncus condimentum, turpis nisl scelerisque turpis, ut gravida eros tortor non arcu. In eget arcu ante. Curabitur lacinia ultricies ante, eget mattis orci vestibulum sit amet. </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
