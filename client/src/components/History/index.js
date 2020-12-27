d022259f72fae6101b7420ba5657e8d5838c0cfc
pocketdru <pocketdru@gmail.com>
2020-12-26 22:52:48 -0700

    dialogs cards added

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -21,6 +21,72 @@ function History() {
                             <div className="col-md-12">
                                 <h2 className="scheme-title">2020</h2>
                             </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-3.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-12">
+                                <h2 className="scheme-title">2019</h2>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
+                                    <div className="card-img-overlay">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-12">
+                                <h2 className="scheme-title">2019</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
