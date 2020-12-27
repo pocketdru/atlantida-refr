36d35e924d31b7ab672ab4564819e80460356c5d
pocketdru <pocketdru@gmail.com>
2020-12-26 23:35:50 -0700

    history content

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -24,9 +24,9 @@ function History() {
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
+                                    <div className="card-img-overlay pr-5">
                                         <h5 className="card-title">Donec id lobortis tellus. Maecenas vel dignissim</h5>
-                                        <p className="card-text date">07 October 2015</p>
+                                        <p className="card-text date">07 October 2020</p>
                                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                                     </div>
                                 </div>
@@ -34,30 +34,31 @@ function History() {
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    <div className="card-img-overlay pl-5">
+                                        <h5 className="card-title">Nam tincidunt, lacus sit amet ultrices </h5>
+                                        <p className="card-text date">05 June 2020</p>
+                                        <p className="card-text">In pulvinar, mi at rhoncus condimentum, turpis nisl scelerisque turpis, ut gravida eros tortor non arcu. In eget arcu ante. Curabitur lacinia ultricies ante, eget mattis orci vestibulum sit amet. Donec quis faucibus lorem. </p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    <div className="card-img-overlay pr-5">
+                                        <h5 className="card-title">Donec quis faucibus lorem</h5>
+                                        <p className="card-text data">07 May 2020</p>
+                                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-3.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    <div className="card-img-overlay pl-5">
+                                        <h5 className="card-title" id="white">Nunc efficitur laoreet turpis sed auctor.</h5>
+                                        <p className="card-text data" id="white">02 April 2020</p>
+                                        <p className="card-text" id="white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Praesent id augue in risus molestie euismod molestie id augue. Suspendisse a libero porttitor, lobortis eros ac, varius velit. Praesent sed eros at elit suscipit laoreet vitae condimentum risus.</p>
                                     </div>
                                 </div>
                             </div>
@@ -67,20 +68,20 @@ function History() {
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    <div className="card-img-overlay pr-5">
+                                        <h5 className="card-title">Donec id lobortis tellus. Maecenas vel dignissim</h5>
+                                        <p className="card-text data">07 October 2019</p>
+                                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="card">
                                     <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p className="card-text">Last updated 3 mins ago</p>
+                                    <div className="card-img-overlay pl-5">
+                                        <h5 className="card-title">Nam tincidunt, lacus sit amet ultrices </h5>
+                                        <p className="card-text data">05 June 2019</p>
+                                        <p className="card-text">In pulvinar, mi at rhoncus condimentum, turpis nisl scelerisque turpis, ut gravida eros tortor non arcu. In eget arcu ante. Curabitur lacinia ultricies ante, eget mattis orci vestibulum sit amet. Donec quis faucibus lorem.</p>
                                     </div>
                                 </div>
                             </div>
