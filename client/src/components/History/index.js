5778dd43aaf934a7ea352dfe67b478e71a940d80
pocketdru <pocketdru@gmail.com>
2021-01-16 23:59:49 -0700

    card responsive

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -5,7 +5,7 @@ import WavySvg from "../Waves";
 function History() {
     return (
         <section className="history">
-            <div className="container pt-5 pb-5">
+            <div className="container pt-3 pb-5">
                 <div className="row">
                     <div className="col-md-12">
                         <div className="row">
@@ -19,13 +19,12 @@ function History() {
                         </div>
                         <div className="row">
                                 <div className="col-md-12">
-                                    <h2 className="scheme-title">2020</h2>
+                                    <h2 className="scheme-title mt-5">2020</h2>
                                 </div>
                             <div className="row pt-5">
                             <div className="col-md-6">
                                 <div className="card">
-                                    <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay pr-5">
+                                    <div className="card-body pr-5">
                                         <h5 className="card-title">Donec id lobortis tellus. Maecenas vel dignissim</h5>
                                         <p className="card-text date">07 October 2020</p>
                                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
@@ -34,8 +33,7 @@ function History() {
                             </div>
                             <div className="col-md-6">
                                 <div className="card mt-5">
-                                    <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay pl-5">
+                                    <div className="card-body pl-5">
                                         <h5 className="card-title">Nam tincidunt, lacus sit amet ultrices </h5>
                                         <p className="card-text date">05 June 2020</p>
                                         <p className="card-text">In pulvinar, mi at rhoncus condimentum, turpis nisl scelerisque turpis, ut gravida eros tortor non arcu. In eget arcu ante. Curabitur lacinia ultricies ante, eget mattis orci vestibulum sit amet. Donec quis faucibus lorem. </p>
@@ -43,20 +41,18 @@ function History() {
                                 </div>
                             </div>
                             <div className="col-md-6">
-                                <div className="card mt">
-                                    <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay pr-5">
+                                <div className="card mt-4">
+                                    <div className="card-body pr-5">
                                         <h5 className="card-title">Donec quis faucibus lorem</h5>
                                         <p className="card-text data">07 May 2020</p>
                                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-md-6">
-                                <div className="card mt-5">
-                                    <img src="/images/history/Border-3.png" className="card-img" alt="..."/>
-                                    <div className="card-img-overlay pl-5">
-                                        <h5 className="card-title" id="white">Nunc efficitur laoreet turpis sed auctor.</h5>
+                            <div className="col-md-6 pt-4">
+                                <div className="card mt-5" id="white">
+                                    <div className="card-body pl-5">
+                                        <h5 className="card-title">Nunc efficitur laoreet turpis sed auctor.</h5>
                                         <p className="card-text data" id="white">02 April 2020</p>
                                         <p className="card-text" id="white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                         Praesent id augue in risus molestie euismod molestie id augue. Suspendisse a libero porttitor, lobortis eros ac, varius velit. Praesent sed eros at elit suscipit laoreet vitae condimentum risus.</p>
@@ -64,14 +60,13 @@ function History() {
                                 </div>
                             </div>
                             </div>
-                                <div className="col-md-12 mt-4 mb-5 pt-2">
+                                <div className="col-md-12 mt-4 mb-4">
                                     <h2 className="scheme-title">2019</h2>
                                 </div>
                             <div className="row">
                                 <div className="col-md-6">
                                     <div className="card">
-                                        <img src="/images/history/Border-1.png" className="card-img" alt="..."/>
-                                        <div className="card-img-overlay pr-5">
+                                        <div className="card-body pr-5">
                                             <h5 className="card-title">Donec id lobortis tellus. Maecenas vel dignissim</h5>
                                             <p className="card-text data">07 October 2019</p>
                                             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed ipsum pulvinar, consequat ipsum non, sodales augue. Donec dapibus ipsum rutrum congue egestas. Curabitur fringilla tortor non aliquam congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
@@ -80,8 +75,7 @@ function History() {
                                 </div>
                                 <div className="col-md-6">
                                     <div className="card mt-5">
-                                        <img src="/images/history/Border-2.png" className="card-img" alt="..."/>
-                                        <div className="card-img-overlay pl-5">
+                                        <div className="card-body pl-5">
                                             <h5 className="card-title">Nam tincidunt, lacus sit amet ultrices </h5>
                                             <p className="card-text data">05 June 2019</p>
                                             <p className="card-text">In pulvinar, mi at rhoncus condimentum, turpis nisl scelerisque turpis, ut gravida eros tortor non arcu. In eget arcu ante. Curabitur lacinia ultricies ante, eget mattis orci vestibulum sit amet. Donec quis faucibus lorem.</p>
