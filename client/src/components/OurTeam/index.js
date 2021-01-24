e9a332d759b5da802a5914352accdf2397065b09
pocketdru <pocketdru@gmail.com>
2021-01-23 22:02:42 -0700

    our team skeleton

--- client/src/components/OurTeam/index.js
+++ client/src/components/OurTeam/index.js
@@ -6,7 +6,49 @@ import { func } from "prop-types";
 function OurTeam () {
     return (
         <section className="ourteam">
-
+            <div className="container pt-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div>
+                                    <h2 className="card-title text-center">Our Team
+                                    <span><WavySvg/></span></h2>
+                                    <p className="card-text text-center">Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                        <div className="card-group">
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
