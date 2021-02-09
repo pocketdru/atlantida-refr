22a9df830bfb457979d1fddc57c8c6ddeb7dfa3c
unknown <pocketdru@gmail.com>
2021-02-09 15:53:23 -0700

    prices grid

--- client/src/components/Prices/index.js
+++ client/src/components/Prices/index.js
@@ -18,6 +18,33 @@ function Prices() {
                                 </div>
                             </div>
                         </div>
+                        <div className="row row-cols-1 row-cols-md-3 g-4 mt-5">
+                            <div className="col">
+                                <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Basic</h5>
+                                    <img src="" />
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Advanced</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Pro</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
