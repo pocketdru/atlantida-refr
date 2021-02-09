b8a4a17b358028e4e8415a74bb1680b35b57f8d9
pocketdru <pocketdru@gmail.com>
2021-02-09 00:08:56 -0700

    proces created and imported

---
+++ client/src/components/Prices/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import WavySvg from "../Waves";
+
+function Prices() {
+    return(
+        <section className="prices">
+
+        </section>
+    )
+}
+
+export default Prices;
