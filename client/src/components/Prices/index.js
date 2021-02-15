c030d7331d9b30b93bafb27cdcb938deef9b36d1
unknown <pocketdru@gmail.com>
2021-02-15 13:05:35 -0700

    image before

--- client/src/components/Prices/index.js
+++ client/src/components/Prices/index.js
@@ -14,7 +14,6 @@ function Prices() {
                                     <h2 className="card-title text-center">Prices
                                     <span><WavySvg/></span></h2>
                                     <p className="card-text text-center">Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. </p>
-                                    <p className="card-text text-center"><i>Hover the cards below</i></p>
                                 </div>
                             </div>
                         </div>
@@ -23,7 +22,9 @@ function Prices() {
                                 <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">Basic</h5>
-                                        <img src="/images/prices/icon-1.png" alt="gallery-photo" className="scheme-title scheme-title-img p-1"/>          
+                                    <div className="scheme-title-img">
+                                        <img src="/images/prices/icon-1.png" alt="gallery-photo" className="scheme-title scheme-title-img"/>        
+                                    </div>  
                                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 </div>
                                 </div>
@@ -32,8 +33,9 @@ function Prices() {
                                 <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">Advanced</h5>
-                                        <img src="/images/prices/icon-2.png" alt="gallery-photo" className="scheme-title scheme-title-img p-1"/>   
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div className="scheme-title-img">
+                                        <img src="/images/prices/icon-1.png" alt="gallery-photo" className="scheme-title scheme-title-img"/>        
+                                    </div>                                      <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 </div>
                                 </div>
                             </div>
@@ -41,8 +43,9 @@ function Prices() {
                                 <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">Pro</h5>
-                                        <img src="/images/prices/icon-3.png" alt="gallery-photo" className="scheme-title scheme-title-img p-1"/>  
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div className="scheme-title-img">
+                                        <img src="/images/prices/icon-1.png" alt="gallery-photo" className="scheme-title scheme-title-img"/>        
+                                    </div>                                      <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 </div>
                                 </div>
                             </div>
