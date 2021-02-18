6ab79563a908a21aa88d10d4aae33d2137e8a6dc
pocketdru <pocketdru@gmail.com>
2021-02-18 00:43:35 -0700

    quality content

--- client/src/components/Quality/index.js
+++ client/src/components/Quality/index.js
@@ -7,10 +7,18 @@ function Quality() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-2">
-                        <img src="/images/quality/Image.png" class="img-fluid" alt="compass"/>
+                        <img src="/images/quality/Image.png" className="img-fluid" alt="compass"/>
                     </div>
-                    <div className="col-md-7"></div>
-                    <div className="col-md-3"></div>
+                    <div className="col-md-7">
+                        <h4 className="text-center">
+                        Looking for a quality and affordable for your next project ?
+                        </h4>
+                        <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu libero ut erat sodales finibus sed in dolor. 
+            Etiam iaculis, nibh a lobortis egestas, est urna sodales</p>
+                    </div>
+                    <div className="col-md-3">
+                        <button type="button" className="btn">Purchase</button>
+                    </div>
                 </div>
             </div>
         </section>
