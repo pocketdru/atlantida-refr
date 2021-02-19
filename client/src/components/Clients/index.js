56ccdd02438c4506950d10e69cfbad1d434922a4
unknown <pocketdru@gmail.com>
2021-02-19 16:01:12 -0700

    blog content

--- client/src/components/Clients/index.js
+++ client/src/components/Clients/index.js
@@ -4,8 +4,44 @@ import WavySvg from "../Waves";
 
 function Clients() {
     return(
-        <section>
-
+        <section className="clients">
+            <div className="container pt-5 pb-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div>
+                                    <h2 className="card-title text-center">Clients
+                                    <span><WavySvg/></span></h2>
+                                    <p className="card-text text-center">Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row-cols-1 row-cols-md-4 g-4">
+                            <div className="col">
+                                <div className="card">
+                                    <img src="/images/clients/2.png" className="card-img-top" alt="clients-icon"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img src="/images/clients/3.png" className="card-img-top" alt="clients-icon"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img src="/images/clients/4.png" className="card-img-top" alt="clients-icon"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img src="/images/clients/5.png" className="card-img-top" alt="clients-icon"/>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
