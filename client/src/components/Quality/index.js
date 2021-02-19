c54e3b0171d5e52453133577b69df90a8f9ce9b7
pocketdru <pocketdru@gmail.com>
2021-02-18 23:42:24 -0700

    quality, buttons

--- client/src/components/Quality/index.js
+++ client/src/components/Quality/index.js
@@ -4,19 +4,19 @@ import "./style.css";
 function Quality() {
     return(
         <section className="quality">
-            <div className="container">
+            <div className="container pt-5 pb-5">
                 <div className="row">
                     <div className="col-md-2">
                         <img src="/images/quality/Image.png" className="img-fluid" alt="compass"/>
                     </div>
-                    <div className="col-md-7">
-                        <h4 className="text-center">
+                    <div className="col-md-6 quality-title">
+                        <h4 className="text-start">
                         Looking for a quality and affordable for your next project ?
                         </h4>
-                        <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu libero ut erat sodales finibus sed in dolor. 
+                        <p className="text-start quality-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu libero ut erat sodales finibus sed in dolor. 
             Etiam iaculis, nibh a lobortis egestas, est urna sodales</p>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-4 buttons-alignment">
                         <button type="button" className="btn">Purchase</button>
                     </div>
                 </div>
