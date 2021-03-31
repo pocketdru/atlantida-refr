8a0fbda3e2409548b479cbd5c66561620c0c3a25
unknown <pocketdru@gmail.com>
2021-03-31 15:35:53 -0600

    sidebar content

--- client/src/components/SidebarBlog/index.js
+++ client/src/components/SidebarBlog/index.js
@@ -5,16 +5,59 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function SidebarBlog() {
     return (
-        <section className="sidebarBlog">
-            <div className="col-md-2">
-            <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">
-                    <FontAwesomeIcon icon={faSearch}/>
-                </span>
+
+    <div className="col-md-2 sidebarBlog">
+        <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">
+            <FontAwesomeIcon icon={faSearch}/>
+        </span>
             <input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
+        </div>
+        <h5 className="search-title">Latest POSTS</h5>
+            <div class="card">
+            <img class="card-img" src="..." alt="Card image cap"/>
+
+            <div class="card-body">
+                <h5 class="card-title">sit amet pretium odio magna ege</h5>
+                <p class="card-text">Nullam lobortis, turpis lobortis placerat vehicula, ipsum</p>
             </div>
             </div>
-        </section>
+            <div class="card">
+            <img class="card-img" src="..." alt="Card image cap"/>
+
+            <div class="card-body">
+                <h5 class="card-title">sit amet pretium odio magna ege</h5>
+                <p class="card-text">Nullam lobortis, turpis lobortis placerat vehicula, ipsum</p>
+            </div>
+            </div>
+            <div class="card">
+            <img class="card-img" src="..." alt="Card image cap"/>
+
+            <div class="card-body">
+                <h5 class="card-title">sit amet pretium odio magna ege</h5>
+                <p class="card-text">Nullam lobortis, turpis lobortis placerat vehicula, ipsum</p>
+            </div>
+            </div>
+            <div class="card">
+            <img class="card-img" src="..." alt="Card image cap"/>
+
+            <div class="card-body">
+                <h5 class="card-title">sit amet pretium odio magna ege</h5>
+                <p class="card-text">Nullam lobortis, turpis lobortis placerat vehicula, ipsum</p>
+            </div>
+            </div>
+            <h5 className="search-title">categories</h5>
+            <ul class="list-group">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+
+        </div>
+
+
     )
 }
 
