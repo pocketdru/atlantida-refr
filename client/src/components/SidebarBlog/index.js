ae1af436e9f3b81d7423f63b00f691b7450c0125
pocketdru <pocketdru@gmail.com>
2021-04-01 00:42:47 -0600

    sidebar content

--- client/src/components/SidebarBlog/index.js
+++ client/src/components/SidebarBlog/index.js
@@ -6,12 +6,12 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 function SidebarBlog() {
     return (
 
-    <div className="col-md-2 sidebarBlog">
+    <div className="col-md-3 sidebarBlog pt-5">
         <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">
+            <input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
+            <span className="input-group-text" id="basic-addon1">
             <FontAwesomeIcon icon={faSearch}/>
         </span>
-            <input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
         </div>
         <h5 className="search-title">Latest POSTS</h5>
             <div class="card">
@@ -48,13 +48,29 @@ function SidebarBlog() {
             </div>
             <h5 className="search-title">categories</h5>
             <ul class="list-group">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Photography</li>
+                <li class="list-group-item">Website Design</li>
+                <li class="list-group-item">Branding</li>
+                <li class="list-group-item">Flyers</li>
+                <li class="list-group-item">Uncategories</li>
+                <li class="list-group-item">Category 22</li>
             </ul>
-
+            <h5 className="search-title">ARCHIVES</h5>
+            <ul class="list-group">
+                <li class="list-group-item">January 2021</li>
+                <li class="list-group-item">December 2020</li>
+                <li class="list-group-item">November 2020</li>
+                <li class="list-group-item">October 2020</li>
+                <li class="list-group-item">September 2020</li>
+            </ul>
+            <h5 className="search-title">FLICKR PHOTOS</h5>
+            <img src="..." class="img-fluid" alt="flickr"/>
+            <img src="..." class="img-fluid" alt="flickr"/>
+            <img src="..." class="img-fluid" alt="flickr"/>
+            <br/>
+            <img src="..." class="img-fluid" alt="flickr"/>
+            <img src="..." class="img-fluid" alt="flickr"/>
+            <img src="..." class="img-fluid" alt="flickr"/>
         </div>
 
 
