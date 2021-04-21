149c8521d6a55857cf86ef774a141ef5985fabad
unknown <pocketdru@gmail.com>
2021-04-21 15:55:23 -0600

    navbar fixes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,30 +1,38 @@
 import React from "react";
 import logo from "./Icon_Logo.png";
+import Icons from "../Icons";
 import "./style.css";
 
 function Nav() {
   return (
-    <div className="container-fluid nav-background">
-      <div className="row"> 
-      <div className="col-md-12 col-lg-12 col-sm-12">
-      <nav className="nav navbar nav-pills flex-column flex-sm-row">
-            <a className="navbar-brand" href="#carousel">
-                <img src={logo} height="30" className="d-inline-block align-top" alt="" loading="lazy"/>
-            </a>
-            <a className="navbar-brand flex-sm-fill text-sm-center" href="/">Atlantida</a>    
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Home</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">About</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Services</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">History</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Team</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Portfolio</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Prices</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="blog">Blog</a>
-            <a className="nav-link flex-sm-fill text-sm-center" href="/">Contact</a>
-      </nav>
+    <section className="nav-background">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <a className="navbar-brand m-0" href="#carousel">
+              <img src={logo} className="d-inline-block" alt="" loading="lazy"/>
+          </a>
+          <a className="navbar-brand flex-sm-fill text-sm-center" href="/">Atlantida</a>    
+        </div>
+      <div className="col-md-9 col-lg-8 col-sm-9 p-0">
+        <nav className="nav navbar nav-pills d-flex justify-content-center pl-0 pr-0">
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Home</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">About</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Services</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">History</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Team</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Portfolio</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Prices</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="blog">Blog</a>
+          <a className="nav-link flex-sm-fill text-sm-center" href="/">Contact</a>
+          </nav>
+        </div>
+        <div  className="col-md-1">
+          <Icons />
+        </div>
       </div>
-      </div>
-    </div>  
+    </div> 
+    </section> 
   );
 }
 
--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -61,7 +61,7 @@ function Post() {
                                 <img className="card-img" src="/images/post/img-4.png" alt="blog-img"/>
                                     <div className="card-body">
                                         <h5 className="card-title">BRIAN SMITH</h5>
-                                        <p className="card-text">02 Nivenber 2015 at 11:00 pm</p>
+                                        <p className="card-text">02 November 2015 at 11:00 pm</p>
                                         <p className="card-paragraph">Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus.</p>
                                         <p className="text-right card-text">
                                             <span> <FontAwesomeIcon icon={faReply} /> </span>
@@ -74,8 +74,8 @@ function Post() {
                             <div className="card pt-5">
                             <img className="card-img" src="/images/post/img-5.png" alt="blog-img"/>
                                 <div className="card-body">
-                                    <h5 className="card-title">BRIAN SMITH</h5>
-                                    <p className="card-text">02 Nivenber 2015 at 11:00 pm</p>
+                                    <h5 className="card-title">ANGELA ROBERTS</h5>
+                                    <p className="card-text">02 November 2015 at 11:00 pm</p>
                                     <p className="card-paragraph">Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna eget risus.</p>
                                     <p className="text-right card-text">
                                          <span> <FontAwesomeIcon icon={faReply} /> </span>
