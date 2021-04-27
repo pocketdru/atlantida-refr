9221543a14bfbf7a5d2fdd61c1f739420a4687d7
pocketdru <pocketdru@gmail.com>
2021-04-27 00:23:49 -0600

    navbar refractoring

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,13 +8,13 @@ function Nav() {
     <section className="nav-background">
     <div className="container">
       <div className="row">
-        <div className="col-md-2">
+        <div className="col-md-3">
           <a className="navbar-brand m-0" href="#carousel">
               <img src={logo} className="d-inline-block" alt="" loading="lazy"/>
           </a>
           <a className="navbar-brand flex-sm-fill text-sm-center" href="/">Atlantida</a>    
         </div>
-      <div className="col-md-9 col-lg-8 col-sm-9 p-0">
+      <div className="col-md-6 col-lg-6 col-sm-12 p-0">
         <nav className="nav navbar nav-pills d-flex justify-content-center pl-0 pr-0">
           <a className="nav-link flex-sm-fill text-sm-center" href="/">Home</a>
           <a className="nav-link flex-sm-fill text-sm-center" href="/">About</a>
@@ -27,7 +27,7 @@ function Nav() {
           <a className="nav-link flex-sm-fill text-sm-center" href="/">Contact</a>
           </nav>
         </div>
-        <div  className="col-md-1">
+        <div  className="col-md-3">
           <Icons />
         </div>
       </div>
