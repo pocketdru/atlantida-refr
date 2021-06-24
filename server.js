be8b700abfbb110b4c75d3c50f7463b46316aeee
pocketdru <pocketdru@gmail.com>
2021-06-23 21:10:52 -0600

    build

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
